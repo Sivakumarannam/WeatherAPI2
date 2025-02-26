d6af6dd6b0c3eab7181924ae8aef86d117e53070
siva-annam <sivakumarannam143@gmail.com>
2025-02-26 22:40:57 +0530

    Fixed ALLOWED_HOSTS issue for deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn weatherAPI2.wsgi --log-file - 
+web: gunicorn weatherAPI2.wsgi:application --bind 0.0.0.0:$PORT --log-file -
--- weatherAPI2/urls.py
+++ weatherAPI2/urls.py
@@ -14,16 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import path
+from django.http import JsonResponse  # Import JsonResponse
+from django.conf import settings  # Import settings for static files
+from django.conf.urls.static import static
 from app import views
 from app.views import *
-from django.conf.urls.static import static
 
+# Health check endpoint
+def health_check(request):
+    return JsonResponse({"status": "ok"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),  # Redirect root URL to home view
+    path('', home, name='home'),
     path('home/', home, name='home'),
     path('registration/', registration, name='registration'),
     path('user_login/', user_login, name='user_login'),
@@ -32,7 +38,8 @@ urlpatterns = [
     path('change_password/', change_password, name='change_password'),
     path('search/', search, name='search'),
     path("reset_password/", reset_password, name="reset_password"),
-    path('send_otp/', send_otp, name='send_otp'),  # Ensure this is here
+    path('send_otp/', send_otp, name='send_otp'),
     path('verify_otp/', verify_otp, name='verify_otp'),
     path('profile_update/', views.profile_update, name='profile_update'),
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path("healthz/", health_check),  # Health check endpoint
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # Serve media files
