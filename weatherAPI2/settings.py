840d4af8c9a0caee116ca6d4b3168137527d53d0
siva-annam <sivakumarannam143@gmail.com>
2025-03-01 23:41:52 +0530

    Updated settings.py

--- weatherAPI2/settings.py
+++ weatherAPI2/settings.py
@@ -116,9 +116,9 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 
 # Cloudinary Configuration
 cloudinary.config( 
-    cloud_name=os.getenv("CLOUD_NAME"), 
-    api_key=os.getenv("API_KEY"), 
-    api_secret=os.getenv("API_SECRET")
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"), 
+    api_key=os.getenv("CLOUDINARY_API_KEY"), 
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
